dee640d6231e236821c2989baee0e4250a2efa6c
Ameryn Hannah Zoë Peters <amerynpeters@gmail.com>
2023-12-14 12:09:17 +0200

    Update login.jsx

--- src/auth/login.jsx
+++ src/auth/login.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
 import { useState } from "react";
 import { supabase } from "../superbase/client";
 import { Link, useNavigate } from "react-router-dom";
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
 
-function Login ({ setToken }) {
-const [formData, setFormData] = useState({
+const styles = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    background: "linear-gradient(to right, #ff7e5f, #feb47b)",
+};
+
+function Login({ setToken }) {
+    const [formData, setFormData] = useState({
         email: "",
         password: "",
     });
 
-
     let navigate = useNavigate();
 
+    function handleInput(e) {
+        e.preventDefault();
+        const { name, value } = e.target;
 
-
+        setFormData((prev) => {
+            return {
+                ...prev,
+                [name]: value,
+            };
+        });
+    }
 
     async function handleSubmit(e) {
         e.preventDefault();
-
 
         try {
             const { data, error } = await supabase.auth.signInWithPassword({
@@ -27,9 +40,7 @@ const [formData, setFormData] = useState({
                 password: formData.password,
             });
 
-
             if (error) throw error;
-
 
             setToken(data);
             navigate("/App");
@@ -39,63 +50,36 @@ const [formData, setFormData] = useState({
         }
     }
 
-return (
-    
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onSubmit={handleSubmit}
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Username!',
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
-  );
+    return (
+        <div style={styles}>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <br />
+                <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInput}
+                />
+                <br />
+                <br />
+                <label htmlFor="password">password</label>
+                <br />
+                <input
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInput}
+                />
+                <br />
+                <br />
+                <button>Login</button>
+            </form>
+            <div>
+                Don't have an account? <Link to="/signup">Sign up</Link>
+            </div>
+        </div>
+    );
 }
 
-
 export default Login;
-
-
